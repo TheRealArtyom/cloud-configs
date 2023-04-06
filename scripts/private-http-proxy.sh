5b2b48c5b1a585783355b84796cd3cc14ec671b1
TheRealArtyom <50754987+TheRealArtyom@users.noreply.github.com>
2023-04-06 17:19:12 +0200

    Update private-http-proxy.sh

--- scripts/private-http-proxy.sh
+++ scripts/private-http-proxy.sh
@@ -3,10 +3,6 @@
 apt update && apt dist-upgrade -y
 apt install -y squid apache2-utils
 
-mkdir /root/.ssh
-echo ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC64hlbFGDRAFm4fbVnNNhNBuev7LMZNrfUe7xkzUvcXgkuJVqSDI86D7rEHtIvriKPi4qB6wzOAKyw4RDPOplRgIXxE461FBFjteh2AxLUiHS8JnbTe/ojctD4pPr1nTH8R4j/foQcy7pb0yA4Lay4bKSiEadp8eczB7STnZh+r5h5pwppRM5HUkxuT+Ur5+bp54Ffe80AazcO+fA3B5+WO9v6eEMIT8jI3hZhdrfrQ6aZUF7B2aIq8HbXm/cHoXjce4+99ggmnYoyb/lmREAV6McbqANHtdvHGGMvtPg1EjQGNa5sRc2BbE2eDIj3wS/uz+vOKXPtk8zOx4/LKbF1 Artjom Ott > /root/.ssh/authorized_keys
-systemctl restart ssh
-
 htpasswd -b -c /etc/squid/passwords artyom sunrise
 systemctl start squid
 systemctl enable squid
