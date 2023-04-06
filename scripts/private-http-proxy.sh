f5c2241e22a72f54b71c48b3d4e61723ceea3e7f
TheRealArtyom <50754987+TheRealArtyom@users.noreply.github.com>
2023-04-06 19:16:12 +0200

    Update private-http-proxy.sh

--- scripts/private-http-proxy.sh
+++ scripts/private-http-proxy.sh
@@ -6,10 +6,15 @@ apt install -y squid apache2-utils
 htpasswd -b -c /etc/squid/passwords artyom sunrise
 systemctl start squid
 systemctl enable squid
+
 sed -i '1403i\auth_param basic program /usr/lib/squid3/basic_ncsa_auth /etc/squid/passwords' /etc/squid/squid.conf
 sed -i '1404i\auth_param basic realm proxy' /etc/squid/squid.conf
 sed -i '1405i\acl authenticated proxy_auth REQUIRED' /etc/squid/squid.conf
 sed -i '1406i\http_access allow authenticated' /etc/squid/squid.conf
+
+sed -i "1399i\acl proxy0 myip $1" /etc/squid/squid.conf
+sed -i "1400i\tcp_outgoing_address $1 proxy0" /etc/squid/squid.conf
+
 systemctl restart squid
 echo "rebooting..."
 reboot now
