9d0f82eb77f7ee007c4178925350508a6e788ead
TheRealArtyom <50754987+TheRealArtyom@users.noreply.github.com>
2023-04-08 02:16:59 +0200

    Update private-http-proxy.sh

--- scripts/private-http-proxy.sh
+++ scripts/private-http-proxy.sh
@@ -1,8 +1,31 @@
 #!/bin/sh
 
+
+# add ssh key
+#mkdir /root/.ssh
+#echo ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC64hlbFGDRAFm4fbVnNNhNBuev7LMZNrfUe7xkzUvcXgkuJVqSDI86D7rEHtIvriKPi4qB6wzOAKyw4RDPOplRgIXxE461FBFjteh2AxLUiHS8JnbTe/ojctD4pPr1nTH8R4j/foQcy7pb0yA4Lay4bKSiEadp8eczB7STnZh+r5h5pwppRM5HUkxuT+Ur5+bp54Ffe80AazcO+fA3B5+WO9v6eEMIT8jI3hZhdrfrQ6aZUF7B2aIq8HbXm/cHoXjce4+99ggmnYoyb/lmREAV6McbqANHtdvHGGMvtPg1EjQGNa5sRc2BbE2eDIj3wS/uz+vOKXPtk8zOx4/LKbF1 Artjom Ott > /root/.ssh/authorized_keys
+#systemctl restart ssh
+
+
 # update the system
 apt update && apt dist-upgrade -y
-apt install -y squid apache2-utils
+apt install -y squid apache2-utils ufw
+
+
+# apply firewall
+ufw --force enable && ufw allow ssh
+
+
+# install node exporter
+wget https://github.com/prometheus/node_exporter/releases/download/v1.5.0/node_exporter-1.5.0.linux-amd64.tar.gz
+tar xvfz node_exporter-1.5.0.linux-amd64.tar.gz
+mv node_exporter-1.5.0.linux-amd64 /opt/node_exporter
+wget https://github.com/TheRealArtyom/cloud-configs/blob/main/services/node_exporter.service -O /etc/systemd/system/node_exporter.service
+systemctl daemon-reload
+systemctl enable node_exporter.service
+systemctl start node_exporter.service
+ufw allow from 94.130.227.146 proto tcp to any port 9100
+
 
 # apply custom network config, we don't need ipv6
 #echo "auto lo" > /etc/network/interfaces.d/50-cloud-init
@@ -14,29 +37,38 @@ apt install -y squid apache2-utils
 #echo "    accept_ra 0" >> /etc/network/interfaces.d/50-cloud-init
 #echo "    mtu 1500" >> /etc/network/interfaces.d/50-cloud-init
 #echo "" >> /etc/network/interfaces.d/50-cloud-init
-
 #systemctl restart networking
+
 
 # create user
 htpasswd -b -c /etc/squid/passwords marius kTH9fZjQAgyDWewVxeyszsUe7UzaeQ85
 systemctl start squid
 systemctl enable squid
 
-# add authentication
+
+# add proxy authentication
 sed -i '1403i\auth_param basic program /usr/lib/squid3/basic_ncsa_auth /etc/squid/passwords' /etc/squid/squid.conf
 sed -i '1404i\auth_param basic realm proxy' /etc/squid/squid.conf
 sed -i '1405i\acl authenticated proxy_auth REQUIRED' /etc/squid/squid.conf
 sed -i '1406i\http_access allow authenticated' /etc/squid/squid.conf
 
+
 # add default ip
 #sed -i "1399i\acl proxy0 myip $1" /etc/squid/squid.conf
 #sed -i "1400i\tcp_outgoing_address $1 proxy0" /etc/squid/squid.conf
 
+
 # restart 
 systemctl restart squid
+
 
 # download our tools
 #wget https://raw.githubusercontent.com/TheRealArtyom/cloud-configs/main/scripts/add-ip.sh
 
-echo "rebooting..."
+
+echo ""
+echo ""
+echo ""
+echo ""
+echo "done. rebooting..."
 reboot now
