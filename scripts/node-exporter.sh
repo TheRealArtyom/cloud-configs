22c6fd1dd1b8f49cb1877b498b1aad2d997f5c24
TheRealArtyom <50754987+TheRealArtyom@users.noreply.github.com>
2023-04-08 23:43:35 +0200

    Update node-exporter.sh

--- scripts/node-exporter.sh
+++ scripts/node-exporter.sh
@@ -3,7 +3,7 @@
 wget https://github.com/prometheus/node_exporter/releases/download/v1.5.0/node_exporter-1.5.0.linux-amd64.tar.gz
 tar xvfz node_exporter-1.5.0.linux-amd64.tar.gz
 mv node_exporter-1.5.0.linux-amd64 /opt/node_exporter
-wget https://github.com/TheRealArtyom/cloud-configs/blob/main/services/node_exporter.service -O /etc/systemd/system/node_exporter.service
+wget https://raw.githubusercontent.com/TheRealArtyom/cloud-configs/main/services/node_exporter.service -O /etc/systemd/system/node_exporter.service
 systemctl daemon-reload
 systemctl enable node_exporter.service
 systemctl start node_exporter.service
